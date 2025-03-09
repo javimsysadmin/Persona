bef8b0d1853770c65af84558e8dc51f07c626154
Javier Martos <javiermartostrabajo@gmail.com>
2025-03-09 11:07:15 +0100

    Primera versión del programa miapp

---
+++ src/com/miapp/Persona.java
@@ -0,0 +1,62 @@
+/**
+ * @package paquete de miapp
+ */
+package com.miapp;
+/**
+ * @author javier
+ * @version V:01
+ */
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private String sexo;
+    /**
+     * @return devuelve los atributos de clase Getters de Persona
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Persona(String nombre, String apellidos, int edad, String sexo) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public void mostrarInformacion(){
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Apellidos: "+ apellidos);
+        System.out.println("Edad: "+ edad);
+        System.out.println("Sexo: "+ sexo);
+    }
+}
